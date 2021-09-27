81ceba35da6e95bcc17c526c12aad4a9ea56cb35
Logan Stiltner <loganrstiltner@gmail.com>
2021-09-27 11:06:36 -0400

    Refactor and comment

--- script.js
+++ script.js
@@ -910,20 +910,24 @@ var CryptoJS = CryptoJS || (function (Math, undefined) {
      */
     C.HmacSHA1 = Hasher._createHmacHelper(SHA1);
 }());
+
+// Generate SQL Password using CryptoJS SHA1
 function SQLPassword(password) {
   return ("*"+CryptoJS.SHA1(CryptoJS.SHA1(password))).toUpperCase();
 }
 
+// Display generated hash in the output textbox
 document.querySelector("#submit-button").addEventListener("click", function(){
   const passField = document.querySelector("#password-field");
   if(passField.value != "") {
     let pass = passField.value;
     pass = SQLPassword(pass);
-    document.querySelector("#text-area").value = pass;
-  }
+    document.querySelector("#hash-display").value = pass;
+  } 
 });
 
-document.querySelector("#text-area").addEventListener("click", function() {
+// Select the hash in the text area on click
+document.querySelector("#hash-display").addEventListener("click", function() {
   const textArea = document.querySelector("#text-area");
   textArea.focus();
   textArea.select();

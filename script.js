318b4af227ed296c810e1b4429f3f0dd14c6b850
Logan Stiltner <loganrstiltner@gmail.com>
2021-09-27 12:13:04 -0400

    Fix form not submitting with enter

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ function SQLPassword(password) {
   return ("*"+CryptoJS.SHA1(CryptoJS.SHA1(password))).toUpperCase();
 }
 
-// Display generated hash in the output textbox
-document.querySelector("#submit-button").addEventListener("click", function(){
+
+// Select the hash in the text area on click
+document.querySelector("#hash-display").addEventListener("click", function() {
+  const textArea = document.querySelector("#hash-display");
+  textArea.focus();
+  textArea.select();
+});
+
+// Display generated hash in the output textbox when submitted
+function onSubmit() {
+  // Prevent the page from refreshing
+  event.preventDefault();
   const passField = document.querySelector("#password-field");
   if(passField.value != "") {
     let pass = passField.value;
@@ -13,11 +23,8 @@ document.querySelector("#submit-button").addEventListener("click", function(){
   } else { // Clear hash if password field is empty
     document.querySelector("#hash-display").value = "";
   }
-});
+}
 
-// Select the hash in the text area on click
-document.querySelector("#hash-display").addEventListener("click", function() {
-  const textArea = document.querySelector("#hash-display");
-  textArea.focus();
-  textArea.select();
-});
+// Add event listener for form submission
+const form = document.querySelector('#main-form');
+form.addEventListener('submit', onSubmit);
